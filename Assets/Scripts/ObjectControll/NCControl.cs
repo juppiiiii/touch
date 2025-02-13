1b6857aa204572e97bb0fdc7872004d6c4b9e6a9
seongjpg <dodoboary@gmail.com>
2025-02-13 17:03:48 +0900

    create objects
    .

---
+++ Assets/Scripts/ObjectControll/NCControl.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class NCControl : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("W"))
+        {
+            
+        }
+        if (Input.GetKey("A"))
+        {
+
+        }
+        if ( Input.GetKey("S"))
+        {
+
+        }
+        if (Input.GetKey("D"))
+        {
+
+        }
+    }
+}
