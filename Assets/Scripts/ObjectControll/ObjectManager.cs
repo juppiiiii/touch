7a78270f07b2efcdd32274bb8afe126fbfe8f717
seongjpg <dodoboary@gmail.com>
2025-02-24 16:40:11 +0900

    마우스 움직임 정상화
    마우스의 세로이동이 이상하게 되던걸 raycast 활용하여 수정

--- Assets/Scripts/ObjectControll/ObjectManager.cs
+++ Assets/Scripts/ObjectControll/ObjectManager.cs
@@ -54,21 +54,18 @@ public class ObjectManager : MonoBehaviour {
 			// 드래그 이동 처리
 			if (isDragging)
 			{
-				//selectedRb.useGravity = false;
-				// 현재 마우스 위치
-				Vector3 currentMousePosition = Input.mousePosition;
+				Plane dragPlane = new Plane(Vector3.up, selected.transform.position);
+				Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
-				// 마우스 이동량 계산
-				Vector3 deltaMousePosition = currentMousePosition - lastMousePosition;
+				if (dragPlane.Raycast(ray, out float enter))
+				{
+					Vector3 hitPoint = ray.GetPoint(enter);
+					Vector3 worldDelta = hitPoint - selected.transform.position;
+					worldDelta.y = 0f;
+					selected.transform.position += worldDelta;
+				}
 
-				// 화면 좌표계에서 월드 좌표계로 변환
-				Vector3 worldDelta = ScreenToWorldDelta(deltaMousePosition);
-
-				// 오브젝트 위치 업데이트
-				selected.transform.position += worldDelta;
-
-				// 마우스 이전 위치 업데이트
-				lastMousePosition = currentMousePosition;
+				lastMousePosition = Input.mousePosition;
 			}
 			else
 			{
@@ -95,26 +92,5 @@ public class ObjectManager : MonoBehaviour {
 				}
 			}
 		}
-	}
-
-	// 화면 좌표계의 이동량을 월드 좌표계의 이동량으로 변환하는 함수
-	Vector3 ScreenToWorldDelta(Vector3 screenDelta)
-	{
-		// 오브젝트의 현재 위치를 화면 좌표로 변환
-		Vector3 screenPosition = mainCamera.WorldToScreenPoint(selected.transform.position);
-
-		// 새로운 화면 좌표 계산
-		Vector3 newScreenPosition = screenPosition + screenDelta;
-
-		// 새로운 화면 좌표를 월드 좌표로 변환
-		Vector3 newWorldPosition = mainCamera.ScreenToWorldPoint(newScreenPosition);
-
-		// 월드 좌표계에서의 이동량 계산
-		Vector3 worldDelta = newWorldPosition - selected.transform.position;
-
-		// y축 이동을 없애고, z축 이동으로 대체 (마우스 y 이동을 z축 이동으로 매핑)
-		worldDelta.y = 0f;
-
-		return worldDelta;
 	}
 }
