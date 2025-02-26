f3d4cf7483b3d831d9d48efb2582b3b0208a1164
juheesvt <wnglk62@gmail.com>
2025-02-26 22:02:29 +0900

    refactor: :recycle: 악령/손길 스폰 관련 로직 변경

--- Assets/Scripts/NightEvent/NightEventManager.cs
+++ Assets/Scripts/NightEvent/NightEventManager.cs
@@ -17,8 +17,6 @@ public class EvilSpiritSettings
 {
     public EvilSpirit normalPrefab;
     public EvilSpirit hungryPrefab;
-    public float spawnChance = 0.3f;
-    public float spawnInterval = 30f;
     public float hungrySpawnRate = 0.3f;
 }
 
@@ -27,8 +25,6 @@ public class GhostlyHandSettings
 {
     public GhostlyHand sidePrefab;
     public GhostlyHand ceilingPrefab;
-    public float spawnChance = 0.3f;
-    public float spawnInterval = 45f;
     public float ceilingSpawnRate = 0.7f;
 }
 
@@ -75,9 +71,6 @@ public class NightEventManager : MonoBehaviour
     public void StartNightEvents()
     {
         int currentWave = GameManager.Instance.CurrentWave;
-        
-        // 웨이브 1은 기본적으로 이벤트 없음
-        if (currentWave < 2) return;
 
         // 모든 이벤트 시작 코루틴
         StartEventRoutine(NightEventType.EvilSpirit, currentWave);
@@ -114,18 +107,10 @@ public class NightEventManager : MonoBehaviour
         switch (eventType)
         {
             case NightEventType.EvilSpirit:
-                StartCoroutine(CreateEventSpawnRoutine(
-                    () => SpawnEvilSpirit(wave),
-                    GetScaledChance(evilSpiritSettings.spawnChance, wave),
-                    GetScaledInterval(evilSpiritSettings.spawnInterval, wave),
-                    eventType));
+                SpawnEvilSpirit(wave);
                 break;
             case NightEventType.GhostlyHand:
-                StartCoroutine(CreateEventSpawnRoutine(
-                    () => SpawnGhostlyHand(wave),
-                    GetScaledChance(handSettings.spawnChance, wave),
-                    GetScaledInterval(handSettings.spawnInterval, wave),
-                    eventType));
+                SpawnGhostlyHand(wave);
                 break;
             case NightEventType.Tossing:
                 StartCoroutine(CreateEventSpawnRoutine(
@@ -137,8 +122,8 @@ public class NightEventManager : MonoBehaviour
             case NightEventType.SleepTalking:
                 StartCoroutine(CreateEventSpawnRoutine(
                     () => StartSleepTalkingEvents(wave),
-                    GetScaledChance(sleepTalkingSettings.occurrenceChance, wave),
-                    GetScaledInterval(sleepTalkingSettings.interval, wave),
+                    sleepTalkingSettings.occurrenceChance,
+                    sleepTalkingSettings.interval,
                     eventType));
                 break;
         }
@@ -146,6 +131,12 @@ public class NightEventManager : MonoBehaviour
 
     private IEnumerator CreateEventSpawnRoutine(System.Action spawnAction, float chance, float interval, NightEventType eventType)
     {
+        // 밤 시작 시 즉시 첫 이벤트 체크
+        if (Random.value <= chance)
+        {
+            spawnAction();
+        }
+
         while (GameManager.Instance.IsNight)
         {
             yield return new WaitForSeconds(interval);
@@ -171,40 +162,64 @@ public class NightEventManager : MonoBehaviour
     private void SpawnEvilSpirit(int wave)
     {
         bool isHungry = Random.value > 0.7f;
+        string type = "악령";
         
-        EvilSpirit prefabToSpawn = isHungry ? evilSpiritSettings.hungryPrefab : evilSpiritSettings.normalPrefab;
-        EvilSpirit spawnedSpirit = Instantiate(prefabToSpawn);
+        // 프리팹이 있을 때만 SpiritTypeName 사용
+        if (isHungry && evilSpiritSettings.hungryPrefab != null)
+        {
+            type = evilSpiritSettings.hungryPrefab.SpiritTypeName;
+        }
+        else if (!isHungry && evilSpiritSettings.normalPrefab != null)
+        {
+            type = evilSpiritSettings.normalPrefab.SpiritTypeName;
+        }
         
-        activeEvents.Add(spawnedSpirit);
+        Debug.Log($"[웨이브 {wave}] {type} 출현! (현재 활성 이벤트 수: {activeEvents.Count + 1})");
         
-        spawnedSpirit.OnEventEnded += () => RemoveActiveEvent(spawnedSpirit);
+        if (isHungry && evilSpiritSettings.hungryPrefab != null)
+        {
+            EvilSpirit spawnedSpirit = Instantiate(evilSpiritSettings.hungryPrefab);
+            activeEvents.Add(spawnedSpirit);
+            spawnedSpirit.OnEventEnded += () => RemoveActiveEvent(spawnedSpirit);
+        }
+        else if (!isHungry && evilSpiritSettings.normalPrefab != null)
+        {
+            EvilSpirit spawnedSpirit = Instantiate(evilSpiritSettings.normalPrefab);
+            activeEvents.Add(spawnedSpirit);
+            spawnedSpirit.OnEventEnded += () => RemoveActiveEvent(spawnedSpirit);
+        }
     }
     #endregion
 
     #region 손길 이벤트
     private void SpawnGhostlyHand(int wave)
     {
-        // 천장형과 측면형 중 랜덤 선택
         bool isCeilingType = Random.value < handSettings.ceilingSpawnRate;
+        string type = isCeilingType ? "천장형 손길" : "측면형 손길";
         
-        GhostlyHand prefabToSpawn = isCeilingType ? handSettings.ceilingPrefab : handSettings.sidePrefab;
-        GhostlyHand spawnedHand = Instantiate(prefabToSpawn);
+        Debug.Log($"[웨이브 {wave}] {type} 출현! (현재 활성 이벤트 수: {activeEvents.Count + 1})");
         
-        activeEvents.Add(spawnedHand);
-        
-        spawnedHand.OnEventEnded += () => RemoveActiveEvent(spawnedHand);
+        if (isCeilingType && handSettings.ceilingPrefab != null)
+        {
+            GhostlyHand spawnedHand = Instantiate(handSettings.ceilingPrefab);
+            activeEvents.Add(spawnedHand);
+            spawnedHand.OnEventEnded += () => RemoveActiveEvent(spawnedHand);
+        }
+        else if (!isCeilingType && handSettings.sidePrefab != null)
+        {
+            GhostlyHand spawnedHand = Instantiate(handSettings.sidePrefab);
+            activeEvents.Add(spawnedHand);
+            spawnedHand.OnEventEnded += () => RemoveActiveEvent(spawnedHand);
+        }
     }
     #endregion
 
     #region 뒤척이기 이벤트
     private void TriggerTossingEvent()
     {
-        Debug.Log("뒤척이기 이벤트 발생!");
-        // 여기서 아이가 뒤척이는 애니메이션이나 효과 실행
-        // 예: 카메라 흔들림, 화면 효과, 소리 등
-        
-        // 예시: 타이머 감소 페널티
-        GameManager.Instance.ReduceTimer(tossingSettings.basePenalty);
+        float penalty = tossingSettings.basePenalty;
+        Debug.Log($"[뒤척이기 이벤트] 발생! (페널티: {penalty}초 감소)");
+        GameManager.Instance.ReduceTimer(penalty);
     }
     #endregion
 
@@ -219,19 +234,14 @@ public class NightEventManager : MonoBehaviour
 
     private IEnumerator ActivateSleepTalking(float duration, float penaltyTime)
     {
-        Debug.Log($"잠꼬대 이벤트 시작! (지속시간: {duration}초)");
-        // 잠꼬대 효과 활성화 (UI, 소리 등)
+        Debug.Log($"[잠꼬대 이벤트] 시작! (지속시간: {duration}초)");
         
-        // 대응하지 않으면 페널티
         bool responded = false;
-        
-        // TODO: UI에서 플레이어가 대응 버튼 누르면 responded = true 설정
-        
         yield return new WaitForSeconds(duration);
         
         if (!responded)
         {
-            Debug.Log($"잠꼬대에 대응하지 않아 타이머 {penaltyTime}초 감소!");
+            Debug.Log($"[잠꼬대 이벤트] 대응 실패! (페널티: {penaltyTime}초 감소)");
             GameManager.Instance.ReduceTimer(penaltyTime);
         }
     }
